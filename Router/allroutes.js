655af8f858f776d48f3f24fd8130c8f47f122bc1
Kevin <kbasseyila@gmail.com>
2026-02-11 14:15:22 +0100

    add signup

--- Router/allroutes.js
+++ Router/allroutes.js
@@ -4,7 +4,9 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),    
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/account", "Mon compte", "/pages/account.html"),    
 ];
     
 
